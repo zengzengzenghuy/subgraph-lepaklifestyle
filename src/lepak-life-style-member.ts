5e2c30095276b3ef5841e0e72072637c1db68329
zengzengzenghuy <tayzh2799@gmail.com>
2022-09-12 03:55:29 +0000

    fix: fix bug for only getting the latest event data

--- src/lepak-life-style-member.ts
+++ src/lepak-life-style-member.ts
@@ -8,12 +8,12 @@ import { Member } from "../generated/schema"
 export function handlenewLifestyleMember(event: newLifestyleMember): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = Member.load(event.transaction.from.toHex())
+  let entity = Member.load(event.transaction.hash.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new Member(event.transaction.from.toHex())
+    entity = new Member(event.transaction.hash.toHex())
 
     // Entity fields can be set using simple assignments
     entity.count = BigInt.fromI32(0)
